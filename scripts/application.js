8022405434f5f8df5d2671e0246a41051916b99a
Santu Mahapatra <santu.m@multunus.com>
2014-11-04 11:23:20 +0530

    Refactored Jquery Code

--- scripts/application.js
+++ scripts/application.js
@@ -1,44 +1,88 @@
-var animation = function(){
-  var key = event.which;
-  var target = $(".movable");
-  var cssVal = {};
+var positionLeft = function(temp){
+  return {left: temp.toString() + "px", opacity: 1};
+}
+
+var positionTop = function(temp){
+  return {top: temp.toString() + "px", opacity: 1};
+}
+
+var arrowKeyNotPressed = function(){
+ return {opacity: 0.5};
+}
+
+var calculatePositionDecreasing = function( position, elementSize ){
+  return (position <= elementSize) ? 0 : (position - elementSize);
+}
+
+var calculatePositionIncreasing = function(position, elementSize, windowSize ){
+  return (position < ( windowSize - ( 2 * elementSize ) ) ) ? (position + elementSize) : ( windowSize - elementSize);
+}
+
+var moveElement = function(target, cssVal) {
+  target.animate(cssVal, 5, function(){});
+}
+
+var calculateElementCoordinates = function(target){
   var offset = target.offset();
-  var leftVal = offset.left;
-  var topVal = offset.top;
-  var elWidth = target.width();
-  var elHeight = target.height();
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  var val = 0;
-  switch(key){
-    //Left Key
-    case 37:
-      val = (leftVal <= elWidth) ? 0 : (leftVal - elWidth);
-      cssVal = {left: val.toString() + "px"}
-      break;
+  return {
+    leftVal: offset.left,
+    topVal: offset.top,
+    elementWidth: target.width(),
+    elementHeight: target.height()
+  }
+}
 
-    // Right Key
-    case 39:
-      val = (leftVal < ( windowWidth - ( 2 * elWidth ) ) ) ? (leftVal + elWidth) : ( windowWidth - elWidth);
-      cssVal = {left: val.toString() + "px"}
-      break;
+var calculateWindowSize = function(){
+  return {
+    windowWidth: $(window).width(),
+    windowHeight: $(window).height()
+  }
+}
 
-    // Up Key
-    case 38:
-      val = (topVal <= elHeight) ? 0 : (topVal - elHeight);
-      cssVal = {top: val.toString() + "px"}
-      break;
+var key = {
+  LEFT : 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40
+}
 
-    // Down Key
-    case 40:
-      val = (topVal < ( windowHeight - ( 2 * elHeight ) ) ) ? (topVal + elHeight) : ( windowHeight - elHeight);
-      cssVal = {top: val.toString() + "px"}
-      break;
+var keyController = function(elementCoordinates, windowSize, keycode){
+  var temp;
+  switch(keycode){
+    case key.LEFT:
+      temp = calculatePositionDecreasing(elementCoordinates.leftVal, elementCoordinates.elementWidth);
+      return positionLeft(temp);
+
+    case key.RIGHT:
+      temp = calculatePositionIncreasing(elementCoordinates.leftVal, elementCoordinates.elementWidth, windowSize.windowWidth);
+      return positionLeft(temp);
+
+    case key.UP:
+      temp = calculatePositionDecreasing(elementCoordinates.topVal, elementCoordinates.elementHeight);
+      return positionTop(temp);
+
+    case key.DOWN:
+      temp = calculatePositionIncreasing(elementCoordinates.topVal, elementCoordinates.elementHeight, windowSize.windowHeight);
+      return positionTop(temp);
 
     default:
-      break;
+      return arrowKeyNotPressed();
   }
-  target.animate(cssVal, 5, function(){});
+}
+
+var controller = function(target, keycode){
+  var elementCoordinates = calculateElementCoordinates(target);
+  var windowSize = calculateWindowSize();
+  return keyController(elementCoordinates, windowSize, keycode);
+}
+
+
+
+var animation = function(){
+  var key_code = event.which;
+  var target = $(".movable");
+  var cssVal = controller(target, key_code);
+  moveElement(target, cssVal);
 }
 
 $(document).ready(function(){
